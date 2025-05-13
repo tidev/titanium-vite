e9c849611f6f02a6c7304b28562750fc12c7adba
Jan Vennemann <j.vennemann@lambus.com>
2025-05-14 00:46:09 +0200

    chore: remove unused variable

--- packages/vite-plugin-titanium/src/classic/entry.ts
+++ packages/vite-plugin-titanium/src/classic/entry.ts
@@ -3,14 +3,8 @@ import type { Plugin } from "vite";
 const VIRTUAL_ENTRY_ID = 'virtual:titanium/main'
 
 export function virtualEntryPlugin(): Plugin {
-  let isProduction = false
-
   return {
     name: "titanium:classic:entry",
-
-    configResolved(config) {
-      isProduction = config.isProduction
-    },
 
     resolveId(source) {
       return source === VIRTUAL_ENTRY_ID ? `\0${source}` : null;
